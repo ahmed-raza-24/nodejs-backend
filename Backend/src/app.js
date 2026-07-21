71a9df24436243030ff027983053b9eb9cf15a31
Ahmed Raza <ahmedrazalohar24@gmail.com>
2026-07-21 21:32:11 +0530

    initial commit

---
+++ Backend/src/app.js
@@ -0,0 +1,56 @@
+const express = require("express")
+const noteModel = require("./models/note.model")
+
+const app = express()
+
+app.use(express.json())
+
+const notes =[]
+
+app.post("/api/notes", async (req, res)=>{
+    const {title, description} = req.body
+
+    const note = await noteModel.create({
+        title, description
+    })
+
+    res.status(201).json({
+        message: "note created successfulli",
+        note
+    })
+})
+
+app.get("/api/notes", async (req, res)=>{
+    const note = await noteModel.find()
+
+    res.status(200).json({
+        message: "Notes fetched sucessfully",
+        note
+    })
+})
+
+app.delete("/api/notes/:id", async (req, res)=>{
+    const id = req.params.id
+
+    await noteModel.findByIdAndDelete(id)
+
+    res.status(200).json({
+        message: "Note deleted successfully"
+    })
+
+})
+
+
+app.patch("/api/notes/:id", async (req, res)=>{
+    const id = req.params.id
+    const {description, title} = req.body
+
+    await noteModel.findByIdAndUpdate(id,{description})
+
+    res.status(200).json({
+        message: "Notes updates sucessfully"
+    })
+})
+
+
+module.exports = app
