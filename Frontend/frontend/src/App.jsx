637b3ca8e4bb8bb67e5bf914bccf19ae761d6263
Ahmed Raza <ahmedrazalohar24@gmail.com>
2026-07-22 20:52:24 +0530

    axios

--- Frontend/frontend/src/App.jsx
+++ Frontend/frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import axios from "axios"
 
 function App() {
   const [notes, setNotes] = useState([{
@@ -20,6 +21,11 @@ function App() {
     description: "test description",
   },
 ])
+
+axios.get('http://localhost:3000/api/notes')
+.then((res)=>{
+  console.log()
+})
 
   return (
     <>
