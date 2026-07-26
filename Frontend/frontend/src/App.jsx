de6da0510c9a3117f485f73f4e9df1b473825963
Ahmed Raza <ahmedrazalohar24@gmail.com>
2026-07-26 20:17:11 +0530

    displaying data from backend to frontend

--- Frontend/frontend/src/App.jsx
+++ Frontend/frontend/src/App.jsx
@@ -17,8 +17,33 @@ function App() {
       })
   }, [])
 
+function handleSubmit(e){
+  e.preventDefault()
+
+  const {title, description} = e.target.elements
+
+  console.log(title.value, description.value)
+
+  axios.post("http://localhost:3000/api/notes",{
+    title:title.value,
+    description:description.value,
+  })
+  .then(res=>{
+    console.log(res.data)
+  })
+
+}
+
+
   return (
     <>
+
+    <form className='note-create-form' onSubmit={handleSubmit}>
+      <input name='title' type='text' placeholder='Enter title'/>
+      <input name='description' type='text' placeholder='Enter description'/>
+      <button>Create note</button>
+    </form>
+
       <div className='notes'>
         {notes.map((note, index) => (
           <div className='note' key={note._id || index}>
