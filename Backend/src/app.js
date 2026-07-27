2fc121f6b37f3f1f673b605c5b5c772a973f72f4
Ahmed Raza <ahmedrazalohar24@gmail.com>
2026-07-27 20:30:45 +0530

    path require

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,6 +1,7 @@
 const express = require("express")
 const noteModel = require("./models/note.model")
 const cors = require("cors")
+const path = require("path")
 
 const app = express()
 app.use(cors())
@@ -53,6 +54,11 @@ app.patch("/api/notes/:id", async (req, res)=>{
         message: "Notes updates sucessfully"
     })
 })
+
+app.use('*name',(req, res)=>{
+    res.sendFile(path.join(__dirname,"/public/index.html"))
+})
+
 
 
 module.exports = app
